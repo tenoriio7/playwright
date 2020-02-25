ccd9dab527fd4d5ac5525bbf772ff33e19881189
Vinicius Lopes <vlopes@uolinc.com>
2020-02-25 14:33:31 -0300

    inserçao de snapshoot

--- test/location_pagseguro.js
+++ test/location_pagseguro.js
@@ -4,7 +4,6 @@ var mapsObject = new Maps()
 describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
         (async () => {
-            console.log(mapsObject)
             for (const browserType of ['chromium',
             //  'firefox',   Error: Geolocation emulation is not supported in Firefox
               'webkit']) {
@@ -17,13 +16,15 @@ describe('#indexOf()', function () {
                 const page = await context.newPage();
                 await page.goto('https://maps.google.com');
                 await page.waitForSelector(mapsObject.tf_search_box);
+                await page.screenshot({ path: `${process.env['HOME']}/workspace/playwright/screenshots/location_tests/${browserType}/home_maps-${browserType}.png`});
                 await page.click(mapsObject.tf_search_box, { waitUntil: 'load' });
                 await page.keyboard.type('PagSeguro UOL - Avenida Brigadeiro Faria Lima - Jardim Paulistano, São Paulo - SP');
                 await page.waitForSelector(mapsObject.bt_search_box_search);
+                await page.screenshot({ path: `${process.env['HOME']}/workspace/playwright/screenshots/location_tests/${browserType}/search_pagseguro-${browserType}.png`});
                 await page.click(mapsObject.bt_search_box_search, { waitUntil: 'load' });
                 await page.waitForSelector(mapsObject.tf_routes, { waitUntil: 'load' });
                 await page.click(mapsObject.tf_routes, { waitUntil: 'load' });
-                await page.screenshot({ path: `${process.env['HOME']}/workspace/playwright/screenshots/location_tests/${browserType}/example-${browserType}.png`});
+                await page.screenshot({ path: `${process.env['HOME']}/workspace/playwright/screenshots/location_tests/${browserType}/pagseguro_result-${browserType}.png`});
                 await browser.close();
             }
         })();
