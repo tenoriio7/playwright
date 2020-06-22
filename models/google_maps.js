190d815f7800d3e811425652dd5b13cb2cfc7b98
Vinicius Tenorio da Silveira Lopes <vlopes@uolinc.com>
2020-06-22 12:08:36 -0300

    inserçao de script

--- models/google_maps.js
+++ models/google_maps.js
@@ -3,7 +3,7 @@ class Maps {
     constructor() {
         this.tf_search_box= '#searchboxinput';
         this.bt_search_box_search = '#searchbox-searchbutton';
-        this.tf_routes = '[data-value="Rotas"]';  
+        this.tf_routes = '[data-value="Routes"]';  
     }
 }
 
