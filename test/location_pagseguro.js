f843aaaa4bd9160293ad261f8ef71ce3c97bd1b6
Vinicius Tenorio da Silveira Lopes <vlopes@uolinc.com>
2020-07-02 16:41:54 -0300

    inserçao de testes com helper e playwright video

--- test/location_pagseguro.js
+++ test/location_pagseguro.js
@@ -8,33 +8,6 @@ var qaData = new QA()
 
 describe('Location Test', function () {
     it('should search PagSeguro in Google', function () {
-        (async () => {
-            for (const browserType of ['chromium', 'firefox']) {
-                const browser = await playwright[browserType].launch({
-                    headless: false, ignoreHTTPSErrors: true, args: [
-                        '--start-maximized' // you can also use '--start-fullscreen'
-                    ]
-                });
-                const context = await browser.newContext();
-                const page = await context.newPage();
-                dimensions = await Helper.load_dimensions(page)
-                await page.setViewportSize({
-                    width: dimensions.width,
-                    height: dimensions.height,
-                  });
-                await page.goto(qaData.maps_uri, { waitUntil: 'load' });
-                await page.waitForSelector(mapsObject.tf_search_box);
-                await page.screenshot({ path: `${process.env['HOME']}/workspace/playwright/screenshots/location_tests/${browserType}/home_maps-${browserType}.png` }, { waitUntil: 'load' });
-                await page.click(mapsObject.tf_search_box, { waitUntil: 'load' });
-                await page.keyboard.type('PagSeguro UOL - Avenida Brigadeiro Faria Lima - Jardim Paulistano, São Paulo - SP');
-                await page.waitForSelector(mapsObject.bt_search_box_search);
-                await page.screenshot({ path: `${process.env['HOME']}/workspace/playwright/screenshots/location_tests/${browserType}/search_pagseguro-${browserType}.png` });
-                await page.click(mapsObject.bt_search_box_search, { waitUntil: 'load' });
-                await page.waitForSelector(mapsObject.tf_routes, { waitUntil: 'load' });
-                await page.click(mapsObject.tf_routes, { waitUntil: 'load' });
-                await page.screenshot({ path: `${process.env['HOME']}/workspace/playwright/screenshots/location_tests/${browserType}/pagseguro_result-${browserType}.png` });
-                await browser.close();
-            }
-        })();
+
     });
 });
--- test/test_with_video.js
+++ test/test_with_video.js
@@ -1,19 +1,22 @@
-// @ts-check
 const { chromium } = require("playwright");
 const { saveVideo } = require("playwright-video");
 
+describe('Record Video Test', function () {
+    it('Record video Test', function () {
+      (async () => {
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        const capture = await saveVideo(page, "video.mp4");
+      
+        await page.goto("https://github.com");
+        await page.type('input[name="q"]', "Playwright");
+        await page.press('input[name="q"]', "Enter");
+        await page.click(".repo-list-item:nth-child(1) a");
+        await capture.stop();
+        await browser.close();
+      })();
+      
+    });
+});
 
-(async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  const capture = await saveVideo(page, "video.mp4");
-
-  await page.goto("https://github.com");
-  await page.type('input[name="q"]', "Playwright");
-  await page.press('input[name="q"]', "Enter");
-  await page.click(".repo-list-item:nth-child(1) a");
-
-  await capture.stop();
-  await browser.close();
-})();
